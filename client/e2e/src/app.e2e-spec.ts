489bc197bae8a1218a639d29f8bc87d54fcf95eb
Cristian Perez <crperez.informatica@gmail.com>
2018-12-24 17:36:40 +0100

    fix e2e tests 5

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,16 @@
-import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('ILO Status in Localhost');
+  it('should display welcome message', async () => {
+    await browser.get('/');
+    console.info('navigated to main')
+    await browser.wait(async () => {}, 5000);
+    let text = await element(by.css('app-root h1')).getText();
+    expect(text).toEqual('ILO Status in Localhost');
   });
 });
+
+
+
+ 
+
