692d7bb04445dab624a077f4d8d69b05ece39fda
Cristian Perez <crperez.informatica@gmail.com>
2018-12-24 17:42:37 +0100

    fix e2e tests 6

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,17 @@
 import { browser, by, element } from 'protractor';
+var originalTimeout;
+beforeEach(function() {
+  originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 90000;
+});
+afterEach(function() {
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+});
 
-describe('workspace-project App', () => {
-  it('should display welcome message', async () => {
+describe('workspace-project App', function() {
+  it('should display welcome message', async function() {
     await browser.get('/');
-    console.info('navigated to main')
-    await browser.wait(async () => {}, 5000);
     let text = await element(by.css('app-root h1')).getText();
     expect(text).toEqual('ILO Status in Localhost');
   });
 });
-
-
-
- 
-
