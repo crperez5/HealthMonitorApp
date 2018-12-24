2f1ab0b256dd5c52bf04b056a7607e090e50a6b5
Cristian Perez <crperez.informatica@gmail.com>
2018-12-24 14:18:37 +0100

    fix e2e tests

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -8,7 +8,8 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('ILO Status in Localhost');
+    page.navigateTo().then(() => {
+      expect(page.getTitleText()).toEqual('ILO Status in Localhost');
+    })
   });
 });
