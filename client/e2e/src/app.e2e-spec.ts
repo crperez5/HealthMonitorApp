9ee8556c81fb6b16b3b267d6eb53737ac5697f70
Cristian Perez <crperez.informatica@gmail.com>
2018-12-24 17:54:21 +0100

    fix e2e tests 8

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -11,6 +11,8 @@ afterEach(function() {
 describe('workspace-project App', function() {
   it('should display welcome message', async function() {
     await browser.get('/');
+    let pagecontent =  await browser.getPageSource();
+    console.info(pagecontent)
     let text = await element(by.css('app-root h1')).getText();
     expect(text).toEqual('ILO Status in Localhost');
   });
