ec5fd510ac689b31242e7a2de0d341db4903c34b
Cristian Perez <crperez.informatica@gmail.com>
2018-12-25 11:58:05 +0100

    fix e2e tests 1

--- client/e2e/src/app.e2e-spec.ts
+++ client/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,16 @@
 import { browser, by, element } from 'protractor';
 
-beforeEach(function () {
-  browser.ignoreSynchronization = true;
-});
-afterEach(function () {
-  browser.ignoreSynchronization = false;
-});
+// beforeEach(function () {
+//   browser.ignoreSynchronization = true;
+// });
+// afterEach(function () {
+//   browser.ignoreSynchronization = false;
+// });
 
 describe('workspace-project App', function() {
   it('should display welcome message', async function() {
     await browser.get('/');
     let text = await element(by.css('app-root h1')).getText();
-    expect(text).toEqual('ILO Status in Localhost');
+    expect(text).toEqual('ILO Status in Production');
   });
 });
